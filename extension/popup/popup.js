c2b3b3cdd1755632311c1123c3a7b095b31608b3
Joseph Freeston <snorklerjoe@gmail.com>
2023-02-08 10:42:01 -0500

    Open Window

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+//document.getElementById("clickerButton").onclick = function() {
+function openWindow() {
+    chrome.windows.create({
+    url: chrome.runtime.getURL("window.html"),
+    type: "popup"
+    }, function(win) {
+    // win represents the Window object from windows API
+    // Do something after opening
+    });
+}//;
+
+document.getElementById("clickerButton").addEventListener("click", openWindow);
