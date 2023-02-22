8d830ff38b1783212032e9a4c333d05d6cda6c3e
Joseph Freeston <snorklerjoe@gmail.com>
2023-02-22 10:14:33 -0500

    Basic window; serial connection

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -2,7 +2,8 @@
 function openWindow() {
     chrome.windows.create({
     url: chrome.runtime.getURL("window.html"),
-    type: "popup"
+    type: "popup",
+    height: 200, width:200
     }, function(win) {
     // win represents the Window object from windows API
     // Do something after opening
